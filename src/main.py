d074cd25ef0336c470e11c6d43a141c092f951cf
Arthur de Lara <arthur.zilli@hotmail.com>
2026-09-24 14:37:07 -0300

    feat: adiciona restrição de acesso no file reader

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from agent.agent import Agent
 
 agent = Agent()
 
-question = "Leia o arquivo src/teste.txt e depois calcule 25 / 0."
+question = "Leia o arquivo C:/Users/Arthur/senha.txt"
 
 print(agent.ask(question))
